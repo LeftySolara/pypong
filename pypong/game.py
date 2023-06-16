637f8c6f86403fedf131ae54e7cd627c3245614c
Julianne Adams <julianne@julianneadams.info>
2023-06-15 21:35:46 -0400

    Refactor: move everything into Game object

---
+++ pypong/game.py
@@ -0,0 +1,62 @@
+import pygame
+from player import Player
+from ball import Ball
+
+class Game():
+    SCREEN_WIDTH = 800
+    SCREEN_HEIGHT = 600
+    SCREEN_PADDING = 30
+
+    def __init__(self):
+        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+
+        self.player = Player()
+        self.player.set_y_limit(self.SCREEN_HEIGHT)
+        self.player.set_pos((self.SCREEN_PADDING, self.SCREEN_PADDING))
+
+        self.opponent = Player()
+        self.opponent.set_y_limit(self.SCREEN_HEIGHT)
+        self.opponent.set_pos((self.SCREEN_WIDTH - self.SCREEN_PADDING - self.opponent.WIDTH, self.SCREEN_PADDING))
+
+        self.ball = Ball()
+        self.ball.set_pos((self.SCREEN_WIDTH - self.SCREEN_PADDING - 20, self.SCREEN_PADDING))
+
+        self.all_sprites = pygame.sprite.Group([self.player, self.opponent, self.ball])
+
+        self.running = False
+
+    def process_input(self):
+        for event in pygame.event.get():
+            if (event.type == pygame.QUIT) or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                self.running = False
+                break
+
+        return pygame.key.get_pressed()
+
+    def update_state(self, pressed_keys: pygame.key.ScancodeWrapper):
+        if pressed_keys[pygame.K_UP]:
+            self.player.move(-4)
+        if pressed_keys[pygame.K_DOWN]:
+            self.player.move(4)
+        self.ball.move(pygame.Vector2(-3, 0))
+
+        # TODO: add ball physics
+        colided = pygame.Rect.colliderect(self.player.rect, self.ball.rect)
+        if (colided):
+            print("Collision")
+
+    def render(self):
+        self.screen.fill("Black")
+        self.all_sprites.draw(self.screen)
+
+        pygame.display.update()
+
+    def run(self):
+        self.running = True
+
+        while self.running:
+            pressed_keys = self.process_input()
+            self.update_state(pressed_keys)
+            self.render()
+            self.clock.tick(60)
--- pypong/pypong.py
+++ pypong/pypong.py
@@ -1,62 +1,9 @@
 import pygame
-from player import Player
-from ball import Ball
+from game import Game
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SCREEN_PADDING = 30
+pygame.init
 
-# TODO: Randomize ball's start position
-BALL_START_X = SCREEN_WIDTH - SCREEN_PADDING - 20
-BALL_START_Y = SCREEN_PADDING
-
-pygame.init()
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-clock = pygame.time.Clock()
-
-player = Player()
-player.set_y_limit(SCREEN_HEIGHT)
-player.set_pos((SCREEN_PADDING, SCREEN_PADDING))
-
-opponent = Player()
-opponent.set_y_limit(SCREEN_HEIGHT)
-opponent.set_pos((SCREEN_WIDTH - SCREEN_PADDING - opponent.WIDTH, SCREEN_PADDING))
-
-ball = Ball()
-ball.set_pos((BALL_START_X, BALL_START_Y))
-
-all_sprites = pygame.sprite.Group([player, opponent, ball])
-
-running = True
-
-while running:
-    # Process input
-    for event in pygame.event.get():
-        if (event.type == pygame.QUIT) or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-            running = False
-            break
-
-    pressed_keys = pygame.key.get_pressed()
-
-    # Update game state
-    if pressed_keys[pygame.K_UP]:
-        player.move(-4)
-    if pressed_keys[pygame.K_DOWN]:
-        player.move(4)
-    ball.move(pygame.Vector2(-3, 0))
-
-    collided = pygame.Rect.colliderect(player.rect, ball.rect)
-    if collided:
-        print("Collision")
-
-    # Render
-    screen.fill("Black")
-    all_sprites.draw(screen)
-
-    pygame.display.update()
-
-    clock.tick(60)
-
+game = Game()
+game.run()
 
 pygame.quit()
